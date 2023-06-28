9f27496f442b1b9c1f86b3abed3cf27d2b4e71bf
Murhaf Alawir <94635412+thecarrot123@users.noreply.github.com>
2023-06-28 23:11:42 +0300

    prepare backend to production

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import os
+import uuid
 
 app = Flask(__name__)
 CORS(app)
@@ -10,21 +11,27 @@ def post_data():
     data = request.get_json()
     code = data["message"]
     params = data["params"]
+    filename = str(uuid.uuid4().hex)
     if params:
-        os.system(f'echo "{code}" > temp')
-        os.system(f'echo "{params}" | ./compiler -f temp  > output')
+        os.system(f'echo "{code}" > {filename}')
+        os.system(f'echo "{params}" | ./compiler -f {filename}  > {filename}.out')
     else:
-        os.system(f'echo "{code}" > temp && ./compiler -f temp > output')
+        os.system(f'echo "{code}" > temp && ./compiler -f {filename}  > {filename}.out')
+    os.system(f'rm {filename}')
     output = ""
-    with open("output", "r") as f:
+    with open(f"{filename}.out", "r") as f:
         output = f.read()
     if output [0:18] == "Enter prog params:":
         output = output[19:]
     if output[0:17] == "done interpreting":
         output = output[18:]
     response = {'message': f'{output}'}
+    os.system(f'rm {filename}.out')
     return jsonify(response)
 
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1> Welcome to flask server! </h1>"
 
 if __name__ == '__main__':
     app.run()
