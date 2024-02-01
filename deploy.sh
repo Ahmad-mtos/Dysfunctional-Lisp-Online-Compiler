ec6b496bc837e85237249172b128ebe65929a75b
root <root@murhaf2001.al.lowhost.ru>
2024-02-01 23:21:13 +0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ DOCS_VOLUME="dysfunctional-lisp-online-compiler_docs_volume"
 [[ ! -z "$(docker volume list | grep $FRONT_VOLUME)" ]] && docker volume rm $FRONT_VOLUME
 [[ ! -z "$(docker volume list | grep $DOCS_VOLUME)" ]] && docker volume rm $DOCS_VOLUME
 
-docker-compose up --build
+docker-compose build
+docker-compose up -d
